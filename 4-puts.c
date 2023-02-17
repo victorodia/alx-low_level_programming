1b9a4fbaac0561553d6a7855f6b9840d686cf929
Odia <victor.odia14@gmail.com>
2023-02-16 19:57:31 -0800

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * This program returns 0 always
+*/
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+                return(0);
