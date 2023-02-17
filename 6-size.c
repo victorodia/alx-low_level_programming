c6590fec50c7ca4edd404e554d4af29e5b9ca4ee
Odia <victor.odia14@gmail.com>
2023-02-16 20:24:10 -0800

    6-size.c

---
+++ 6-size.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/*
+*To print out sizes of various types on a computer
+*/
+
+int main(void)
+{
+        printf("Size of a char: %zu byte(s)\n",sizeof(char));
+        printf("Size of a int: %zu byte(s)\n",sizeof(int));        printf("Size of a long int: %zu byte(s)\n",sizeof(long int));
+        printf("Size of a long long int: %zu byte(s)\n",sizeof(long long int));
+        printf("Size of a float: %zu byte(s)\n",sizeof(float));
+        return(0);
+}
