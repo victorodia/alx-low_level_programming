08e821c9130d0e3bb9c07163074f4cb199cfc0ad
Odia <victor.odia14@gmail.com>
2023-02-16 20:48:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /*
- * This program returns 0 always
+* Main - Entry Point
+* This program returns 0 always
 */
 
 int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
                 return(0);
+}
