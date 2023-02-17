83a3f74fd718ba97d2f027c63b8463db0e37351b
Odia <victor.odia14@gmail.com>
2023-02-16 20:46:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /*
- * This program returns 0 always
+*main -Entry point
+* This program returns 0 always
 */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-                return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+       		return(0);
 }
