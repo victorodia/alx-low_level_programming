104ba0f0b4c4ebcdbb83150de7ef9ec774c5e960
Odia <victor.odia14@gmail.com>
2023-02-16 20:03:19 -0800

    5-printf.c

---
+++ 5-printf.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * This program returns 0 always
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+                return(0);
