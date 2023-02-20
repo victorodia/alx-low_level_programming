02328de96d65ade5d7ca71a6bf574cbc779a3644
Odia <victor.odia14@gmail.com>
2023-02-20 14:14:49 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point for the code
+ * Description -  Prints lower cases alphabets without q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
