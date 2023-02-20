4163ddc59fc4de32042b8d6b84ba4f9409660664
Odia <victor.odia14@gmail.com>
2023-02-20 15:10:13 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point for the code
+ * Description -  Prints lower cases alphabets without q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(48 + i)
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	
+	}
+	return (0);
+}
