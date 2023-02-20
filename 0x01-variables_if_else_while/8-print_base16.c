737e52f282ad538cd5809569eec8287db2b50ef1
Odia <victor.odia14@gmail.com>
2023-02-20 14:37:40 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point for the code
+ * Description -  Prints lower cases alphabets without q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
