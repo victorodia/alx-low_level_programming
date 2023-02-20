5d19deeb4c627773c2a08d1d4a0d7a7199eea008
Odia <victor.odia14@gmail.com>
2023-02-20 13:48:14 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point for the code
+ * Description -  Prints lower cases alphabets without q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 1;
+
+	while (a <= 9)
+	{
+		printf(a'\n');
+	}
+		a++;
+	return (0);
+}
