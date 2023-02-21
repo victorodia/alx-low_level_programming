5b2e511f94e0a96a02196908a41cc19905427af9
Odia <victor.odia14@gmail.com>
2023-02-21 14:09:07 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet 10 time
+ */
+
+void print_alphabet_x10(void)
+{
+	char i = 1;
+	char a;
+
+	while (i <= 10)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
