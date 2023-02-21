289c1e8523166374b7b0d6c0add39cc272a109a8
Odia <victor.odia14@gmail.com>
2023-02-21 14:38:57 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ * @c: char to be checked
+ * Returns: 1 (Success) 0(Failure)
+ */
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
