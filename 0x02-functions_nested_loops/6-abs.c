8cae122149f32bf58966f4a73fd966bbdba3be9c
Odia <victor.odia14@gmail.com>
2023-02-21 15:17:16 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - To print the absolute value  of a number
+ * @i:  Value to be checked
+ * Return: n if positive, -n if negative
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
