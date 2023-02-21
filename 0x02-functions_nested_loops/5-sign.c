11299abf8240882662e10cb47ea2d6e1729cd5d8
Odia <victor.odia14@gmail.com>
2023-02-21 15:10:13 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - To print the sign of a number
+ * @n:  Value to be checked
+ * Return: 1 if greater than zero, 0 if zero, -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
