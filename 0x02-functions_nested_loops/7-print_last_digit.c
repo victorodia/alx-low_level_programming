dbb81584b758c6692b7e7cdb5c96a72de53e9ae9
Odia <victor.odia14@gmail.com>
2023-02-21 15:31:20 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @i: Number to be used
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int i)
+{
+	char c;
+
+	if (i < 0)
+	{
+		c = (i % 10);
+		c = (-c);
+		_putchar(c);
+		return (c);
+	}
+	else
+	{
+		c = (i % 10);
+		_putchar(c);
+		return (c);
+	}
+}
