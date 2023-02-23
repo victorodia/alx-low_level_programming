9546f35e3c6565b48bb33e1f8bcbc7fee584a610
Odia <victor.odia14@gmail.com>
2023-02-23 15:18:19 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - CHeeck for Uppercase letters
+ * @i: character to check
+ * Return: 1 if success, 0 if fail
+ */
+
+int _isupper(int i)
+{
+	if (i >= "A" && i <= "Z")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
