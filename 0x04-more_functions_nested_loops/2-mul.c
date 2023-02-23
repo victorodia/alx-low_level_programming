5c7fa56a35334710210febf700821f9efd9b8444
Odia <victor.odia14@gmail.com>
2023-02-23 15:31:23 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
