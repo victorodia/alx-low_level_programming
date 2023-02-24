4f02f7029cf21f4feda94663619aa808b39ef3e7
Odia <victor.odia14@gmail.com>
2023-02-23 18:54:59 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints numbers from 0 - 9
- * Return: void
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
 {
-	char i;
+	char c;
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
-
+		_putchar(c);
 	}
 	_putchar('\n');
 }
+
